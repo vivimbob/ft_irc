0506239b4b35bdfb5c5d031016bb864ab6a68fcc
snpark <atimangel@google.com>
2022-05-16 20:04:28 +0900

    parse IRCMessage prototype

--- includes/IRCMessage.hpp
+++ includes/IRCMessage.hpp
@@ -1,14 +1,33 @@
 #ifndef IRCMESSAGE_HPP
 #define IRCMESSAGE_HPP
 
+#include <string>
+#include <vector>
+
 class IRCMessage
 {
-public:
+private:
+    std::string m_message;
+    std::string m_prefix;
+    std::string m_command;
+    std::vector<std::string> m_parameters;
+    bool m_valid_message; 
+    int m_position;
+
     IRCMessage(void);
+    IRCMessage(const IRCMessage& copy);
+	IRCMessage	&operator=(const IRCMessage& other);
+
+    int next_position(void);
+public:
     IRCMessage(unsigned int clientfd, const std::string &message);
     ~IRCMessage(void);
 
     void    parse_message(void);
+    const std::string   &get_prefix(void) const;
+	const std::string   &get_command(void) const;
+	const std::vector<std::string>  &get_params(void) const;
+    const bool &is_valid_message(void) const;
 };
 
 #endif /* IRCMESSAGE_HPP */
