98382dd022df5578313d403f1ab4f96fee586a6b
Yudongwoo <tkffkels10@gmail.com>
2022-05-16 20:36:08 +0900

    Add some simple functions in channel class

--- includes/channel.hpp
+++ includes/channel.hpp
@@ -8,13 +8,14 @@
 
 class Channel
 {
+  // server에 있는 중첩된 client 클래스를 밖으로 빼고 list vector 관련 구현할 예정.
   private:
-    std::string channel_name;
-    time_t channel_init_time;
-    // std::vector<client> user_lists;
-    // std::vector<client> operator_lists;
-    std::string channel_topic;
-    // std::vector<client> banned_lists;
+    std::string m_channel_name;
+    time_t m_channel_init_time;
+    // std::vector<client> m_user_lists;
+    // std::vector<client> m_operator_lists;
+    std::string m_channel_topic;
+    // std::vector<client> m_banned_lists;
 
     Channel();
     Channel(const Channel& cp);
--- srcs/channel.cpp
+++ srcs/channel.cpp
@@ -1,9 +1,45 @@
 #include "../includes/channel.hpp"
+#include "../includes/logger.hpp"
 
-Channel::Channel(const std::string &name, time_t time) : channel_name(name), channel_init_time(time)
+Channel::Channel(const std::string &name, time_t time)
+ : m_channel_name(name),
+   m_channel_init_time(time)
 {
 }
 
 Channel::~Channel()
 {
+}
+
+const
+  std::string &Channel::get_channel_name() const
+{
+  return m_channel_name;
+}
+
+const
+  std::string &Channel::get_channel_topic() const
+{
+  return m_channel_topic;
+}
+
+void
+  Channel::set_channel_name(const std::string &name)
+{
+  this->m_channel_name = name;
+}
+
+void
+  Channel::set_channel_topic(const std::string &topic)
+{
+  this->m_channel_topic = topic;
+}
+
+void
+  Channel::display_channel_info()
+{
+  // 일단 확인용으로 로거 출력해놓음.
+  Logger().info() << "channel's name : " << this->m_channel_name;
+  Logger().info() << "channel's init time : " << this->m_channel_init_time;
+  Logger().info() << "channel's topic : " << this->m_channel_topic;
 }
