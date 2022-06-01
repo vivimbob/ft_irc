e6cf6672d876acbbfb85ef140bd22433ada31286
minjakim <76225727+minjakim@users.noreply.github.com>
2022-06-01 18:41:20 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@
 int
     main(int argc, char **argv)
 {
-    const std::string timestamp_separator = ": ";
-    Logger::set_timestamp_mode(Logger::Date_time_ver1, timestamp_separator);
-	Logger::set_loglevel(Logger::Trace);
-
     Server(argc, argv).run();
     return 0;
 }
