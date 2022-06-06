388aec279708e1aa684fe659419f43891c76929f
Yudongwoo <tkffkels10@gmail.com>
2022-06-06 20:13:43 +0900

    #161/dyu/Implement names command part when channel is public

--- srcs/commands/names.cpp
+++ srcs/commands/names.cpp
@@ -5,7 +5,6 @@
 void
   Server::m_process_names_command(Client &client, Message &msg)
 {
-  (void)client;
   if (msg.get_params().empty())
   {
     ChannelMap::const_iterator channel_it = m_channel_map.begin();
@@ -16,8 +15,25 @@ void
       Channel *channel = channel_it->second;
       if (channel->is_private_mode() || channel->is_secret_mode())
         asterisk_channel.push_back(channel_name);
-      
+      else
+      {
+        const Channel::MemberMap &user_list = channel->get_user_list();
+        Channel::MemberMap::const_iterator user = user_list.begin();
+        std::queue<const std::string> nick_queue;
+        for (; user != user_list.end(); ++user)
+        {
+            if (user->first->is_invisible())
+            {
+              if (channel->is_operator(user->first))
+                nick_queue.push("@" + user->first->get_nickname());
+              else if (channel->is_voice_mode(user->first))
+                nick_queue.push("+" + user->first->get_nickname());
+              else
+                nick_queue.push(user->first->get_nickname());
+            }
+        }
+        client.push_message(msg.rpl_namreply("=" + channel_name, nick_queue));
+      }
     }
-
   }
 }
