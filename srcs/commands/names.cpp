0ca6593b130e24d69d8a7de118f23911e0caf3a6
Yudongwoo <tkffkels10@gmail.com>
2022-06-06 15:21:46 +0900

    #162/dyu/Add m_process_names_command definition frame in names.cpp

--- srcs/commands/names.cpp
+++ srcs/commands/names.cpp
@@ -1,3 +1,9 @@
 #include "../../includes/logger.hpp"
 #include "../../includes/utils.hpp"
 #include "../../includes/server.hpp"
+
+void Server::m_process_names_command(Client &client, Message &msg)
+{
+  (void)client;
+  (void)msg;
+}
