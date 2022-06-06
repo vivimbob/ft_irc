e2cb9ae361a6373841364bb8e71cb87ace15aa76
Yudongwoo <tkffkels10@gmail.com>
2022-06-06 17:53:25 +0900

    #161/dyu/Store invisible channel names in asterisk_channel vector

--- srcs/commands/names.cpp
+++ srcs/commands/names.cpp
@@ -2,8 +2,22 @@
 #include "../../includes/utils.hpp"
 #include "../../includes/server.hpp"
 
-void Server::m_process_names_command(Client &client, Message &msg)
+void
+  Server::m_process_names_command(Client &client, Message &msg)
 {
   (void)client;
-  (void)msg;
+  if (msg.get_params().empty())
+  {
+    ChannelMap::const_iterator channel_it = m_channel_map.begin();
+    std::vector<std::string> asterisk_channel;
+    for (; channel_it != m_channel_map.end(); ++channel_it)
+    {
+      const std::string &channel_name = channel_it->first;
+      Channel *channel = channel_it->second;
+      if (channel->is_private_mode() || channel->is_secret_mode())
+        asterisk_channel.push_back(channel_name);
+      
+    }
+
+  }
 }
