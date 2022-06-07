183cd08840d3e2be3fcef19b832d6f352c2b062b
Yudongwoo <tkffkels10@gmail.com>
2022-06-07 17:08:40 +0900

    #163/dyu/Replace iterating user_list parts with function named iterate_user_xx not to duplicate them

--- srcs/commands/names.cpp
+++ srcs/commands/names.cpp
@@ -23,6 +23,32 @@ inline void
     nick_queue.push(client.get_nickname());
 }
 
+inline void
+  iterate_user_not_joined(Channel *channel, std::queue<const std::string>& nick_queue, bool is_public = false)
+{
+  const Channel::MemberMap &user_list = channel->get_user_list();
+  Channel::MemberMap::const_iterator user = user_list.begin();
+  for (; user != user_list.end(); ++user)
+  {
+    if (!user->first->is_invisible())
+    {
+      if (is_public)
+        store_nickname_in_queue(channel, *user->first, nick_queue);
+      else
+        nick_queue.push(user->first->get_nickname());
+    }
+  }
+}
+
+inline void
+  iterate_user_joined(Channel *channel, std::queue<const std::string>& nick_queue)
+{
+  const Channel::MemberMap &user_list = channel->get_user_list();
+  Channel::MemberMap::const_iterator user = user_list.begin();
+  for (; user != user_list.end(); ++user)
+    store_nickname_in_queue(channel, *user->first, nick_queue);
+}
+
 void
   Server::m_process_names_command(Client &client, Message &msg)
 {
@@ -35,27 +61,18 @@ void
     {
       const std::string &channel_name = channel_it->first;
       Channel *channel = channel_it->second;
-      const Channel::MemberMap &user_list = channel->get_user_list();
-      Channel::MemberMap::const_iterator user = user_list.begin();
       std::queue<const std::string> nick_queue;
       if (client.is_already_joined(channel)) // 해당 클라이언트가 채널에 가입되어 있을 때
       {
         std::string symbol = get_channel_symbol(channel);
-        for (; user != user_list.end(); ++user)
-          store_nickname_in_queue(channel, *user->first, nick_queue);
+        iterate_user_joined(channel, nick_queue);
         client.push_message(msg.rpl_namreply(symbol + channel_name, nick_queue));
       }
       else if (channel->is_private_mode() || channel->is_secret_mode()) // 가입 안되어있고 채널이 private, secret 모드일 때
         asterisk_channel.push_back(channel_name);
       else // 가입 안되어있고 채널이 public일 때
       {
-        const Channel::MemberMap &user_list = channel->get_user_list();
-        Channel::MemberMap::const_iterator user = user_list.begin();
-        for (; user != user_list.end(); ++user)
-        {
-          if (!user->first->is_invisible())
-            store_nickname_in_queue(channel, *user->first, nick_queue);
-        }
+        iterate_user_not_joined(channel, nick_queue, true);
         client.push_message(msg.rpl_namreply("=" + channel_name, nick_queue));
       }
     }
@@ -65,13 +82,8 @@ void
     std::queue<const std::string> nick_queue;
     for (; asterisk_channel_it != asterisk_channel.end(); ++asterisk_channel_it) // 가입 안되어있고 채널이 private, secret 모드일 때
     {
-      const Channel::MemberMap &user_list = m_channel_map[*asterisk_channel_it]->get_user_list();
-      Channel::MemberMap::const_iterator user = user_list.begin();
-      for (; user != user_list.end(); ++user)
-      {
-        if (!user->first->is_invisible())
-          nick_queue.push(user->first->get_nickname());
-      }
+      Channel *channel = m_channel_map[*asterisk_channel_it];
+      iterate_user_not_joined(channel, nick_queue);
     }
     ClientMap::const_iterator client_it = m_client_map.begin();
     for (;client_it != m_client_map.end(); ++client_it) // 클라이언트가 어느 채널에도 속하지 않을 때
@@ -93,38 +105,23 @@ void
         continue;
       const std::string &channel_name = *channel_it;
       Channel *channel = m_channel_map[channel_name];
-      const Channel::MemberMap &user_list = channel->get_user_list();
-      Channel::MemberMap::const_iterator user = user_list.begin();
       std::queue<const std::string> nick_queue;
       if (client.is_already_joined(channel))
       {
         std::string symbol = get_channel_symbol(channel);
-        for (; user != user_list.end(); ++user)
-          store_nickname_in_queue(channel, *user->first, nick_queue);
+        iterate_user_joined(channel, nick_queue);
         client.push_message(msg.rpl_namreply(symbol + channel_name, nick_queue));
       }
       else
       {
         if (!channel->is_private_mode() && !channel->is_secret_mode())
         {
-          const Channel::MemberMap &user_list = channel->get_user_list();
-          Channel::MemberMap::const_iterator user = user_list.begin();
-          for (; user != user_list.end(); ++user)
-          {
-            if (!user->first->is_invisible())
-              store_nickname_in_queue(channel, *user->first, nick_queue);
-          }
+          iterate_user_not_joined(channel, nick_queue, true);
           client.push_message(msg.rpl_namreply("=" + channel_name, nick_queue));
         }
         else if (channel->is_private_mode())
         {
-          const Channel::MemberMap &user_list = channel->get_user_list();
-          Channel::MemberMap::const_iterator user = user_list.begin();
-          for (; user != user_list.end(); ++user)
-          {
-            if (!user->first->is_invisible())
-              store_nickname_in_queue(channel, *user->first, nick_queue);
-          }
+          iterate_user_not_joined(channel, nick_queue);
           client.push_message(msg.rpl_namreply("*", nick_queue));
         }
         else
