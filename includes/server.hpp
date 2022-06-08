4034513927e3ae7bb439dba08cc27dc100b92d2f
Yudongwoo <tkffkels10@gmail.com>
2022-06-08 14:51:37 +0900

    #176/dyu/Add process_list_command prototype in server.hpp

--- includes/server.hpp
+++ includes/server.hpp
@@ -54,6 +54,7 @@ class Server
         void m_process_user_mode_command(Client &client, Message &msg, const std::string &nickname);
         void m_process_names_command(Client &client, Message &msg);
         void m_process_privmsg_command(Client &client, Message &msg);
+        void m_process_list_command(Client &client, Message &msg);
 
         void m_join_channel(Client &client, Message &msg, std::map<const std::string, const std::string> &chan_key_pair);
         
