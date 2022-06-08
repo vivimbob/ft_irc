254b0f4f795b90dff8ea9ef52ec8e0cb70320ff1
snpark <snpark@student.42seoul.kr>
2022-06-08 16:08:45 +0900

    #179 인자 쪼개고 반복문 만들고, 내부에서 채널인지 체크

--- srcs/commands/privmsg.cpp
+++ srcs/commands/privmsg.cpp
@@ -1,7 +1,29 @@
 #include "../../includes/server.hpp"
+#include "../../includes/logger.hpp"
+#include "../../includes/utils.hpp"
 
 void
     Server::m_process_privmsg_command(Client &client, Message &msg)
 {
-	(void)client; (void)msg;
+	const std::vector<std::string> &parameter = msg.get_params();
+    
+    if (parameter.empty())
+        client.push_message(msg.err_no_recipient(), Logger::Debug);
+    if (parameter.size() == 1)
+        client.push_message(msg.err_no_text_to_send(), Logger::Debug);
+    
+    std::vector<const std::string> target_list;
+    utils::split_by_comma(target_list, parameter[0]);
+
+    std::vector<const std::string>::iterator it = target_list.begin();
+    std::vector<const std::string>::iterator ite = target_list.end();
+    for (;it != ite; ++it)
+    {
+       if (utils::is_channel_prefix(*it))
+       {
+       }
+       else
+       {
+       }
+    }
 }
