d1cd373d5170a0c8c69e9cb29d4199e1a3df1a0d
Yudongwoo <tkffkels10@gmail.com>
2022-06-08 15:37:05 +0900

    #178/dyu/Store channel names coming from message separated by ','in case of one parameter

--- srcs/commands/list.cpp
+++ srcs/commands/list.cpp
@@ -8,10 +8,12 @@ void Server::m_process_list_command(Client &client, Message &msg)
   (void)msg;
   if (msg.get_params().empty())
   {
-
+    
   }
   else if (msg.get_params().size() == 1)
   {
-    
+    std::vector<const std::string> channel_list;
+    utils::split_by_comma(channel_list, msg.get_params()[0]);
+   
   }
 }
