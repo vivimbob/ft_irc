5005c3dc452c0ebcffd95625bda2f562bbda6476
Yudongwoo <tkffkels10@gmail.com>
2022-06-08 15:21:37 +0900

    #178/dyu/Add if-elseif statement depending on the number of paramenter

--- srcs/commands/list.cpp
+++ srcs/commands/list.cpp
@@ -6,4 +6,12 @@ void Server::m_process_list_command(Client &client, Message &msg)
 {
   (void)client;
   (void)msg;
+  if (msg.get_params().empty())
+  {
+
+  }
+  else if (msg.get_params().size() == 1)
+  {
+    
+  }
 }
