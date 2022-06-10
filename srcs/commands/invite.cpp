557aa3c7abc7190d796a8500844a104e4d2741a1
Yudongwoo <tkffkels10@gmail.com>
2022-06-10 12:07:17 +0900

    #194/dyu/Check if there are two parameters coming from message

--- srcs/commands/invite.cpp
+++ srcs/commands/invite.cpp
@@ -6,4 +6,13 @@ void Server::m_process_invite_command(Client &client, Message &msg)
 {
   (void)client;
   (void)msg;
+  if (msg.get_params().size() < 2)
+  {
+    client.push_message(msg.err_need_more_params(), Logger::Debug);
+    return ;
+  }
+  else if (msg.get_params().size() == 2)
+  {
+    
+  }
 }
