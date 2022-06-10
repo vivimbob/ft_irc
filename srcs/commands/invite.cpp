7eab89c8cbe82d69b95042c305a7055027080b88
Yudongwoo <tkffkels10@gmail.com>
2022-06-10 14:21:57 +0900

    #194/dyu/Delete void implicit cast

--- srcs/commands/invite.cpp
+++ srcs/commands/invite.cpp
@@ -4,8 +4,6 @@
 
 void Server::m_process_invite_command(Client &client, Message &msg)
 {
-  (void)client;
-  (void)msg;
   if (msg.get_params().size() < 2)
   {
     client.push_message(msg.err_need_more_params(), Logger::Debug);
