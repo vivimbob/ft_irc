d7b0a792b7e9a96f8e7b84b24d917863da9155aa
Yudongwoo <tkffkels10@gmail.com>
2022-06-10 12:27:19 +0900

    #194/dyu/Rename first_param,second_param nickname,channel_name respectively

--- srcs/commands/invite.cpp
+++ srcs/commands/invite.cpp
@@ -13,16 +13,16 @@ void Server::m_process_invite_command(Client &client, Message &msg)
   }
   else if (msg.get_params().size() == 2)
   {
-    const std::string first_param = msg.get_params()[0];
-    if (!m_client_map.count(first_param))
+    const std::string nickname = msg.get_params()[0];
+    if (!m_client_map.count(nickname))
     {
-      client.push_message(msg.err_no_such_nick(first_param), Logger::Debug);
+      client.push_message(msg.err_no_such_nick(nickname), Logger::Debug);
       return ;
     }
-    const std::string second_param = msg.get_params()[1];
-    if (!m_channel_map.count(second_param))
+    const std::string channel_name = msg.get_params()[1];
+    if (!m_channel_map.count(channel_name))
     {
-      client.push_message(msg.err_no_such_channel(second_param), Logger::Debug);
+      client.push_message(msg.err_no_such_channel(channel_name), Logger::Debug);
       return ;
     }
   }
