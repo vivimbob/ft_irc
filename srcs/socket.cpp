9c81da10e285b7892e597caec473e1093c2b6b30
minjakim <minjakim@student.42seoul.kr>
2022-06-17 17:00:22 +0900

    minjakim

--- srcs/socket.cpp
+++ srcs/socket.cpp
@@ -1,54 +1,50 @@
 #include "../includes/socket.hpp"
 
-Socket::Socket() : _socklen(sizeof(sockaddr_in)), _fd(-1)
-{
-}
-
 void
     Socket::m_create()
 {
-    if ((Socket::_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    if ((_socket.fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         Logger().error() << "Failed to create socket. errno " << errno << ":"
                          << strerror(errno);
         exit(EXIT_FAILURE);
     }
-    Logger().info() << "Create socket " << Socket::_fd;
+    Logger().info() << "Create socket " << _socket.fd;
     int toggle = 1;
-    setsockopt(Socket::_fd, SOL_SOCKET, SO_REUSEPORT, (const void*)&toggle,
+    setsockopt(_socket.fd, SOL_SOCKET, SO_REUSEPORT, (const void*)&toggle,
                sizeof(toggle));
 }
 
 void
     Socket::m_bind(int port)
 {
-    memset(&_sockaddr, 0, sizeof(sockaddr_in));
-    _sockaddr.sin_family      = AF_INET;
-    _sockaddr.sin_addr.s_addr = INADDR_ANY;
-    _sockaddr.sin_port        = htons(port);
+    memset(&_socket.addr, 0, sizeof(sockaddr_in));
+    _socket.addr.sin_family      = AF_INET;
+    _socket.addr.sin_addr.s_addr = INADDR_ANY;
+    _socket.addr.sin_port        = htons(port);
 
-    if (bind(Socket::_fd, (struct sockaddr*)&_sockaddr, sizeof(sockaddr_in)) ==
-        -1)
+    if (bind(_socket.fd, (struct sockaddr*)&_socket.addr,
+             sizeof(sockaddr_in)) == -1)
     {
         Logger().error() << "Failed to bind to port and address" << port
                          << ". errno: " << errno << ":" << strerror(errno);
         exit(EXIT_FAILURE);
     }
     Logger().info() << "Bind Port :" << port
-                    << " IP :" << inet_ntoa(_sockaddr.sin_addr);
+                    << " IP :" << inet_ntoa(_socket.addr.sin_addr);
 }
 
 void
     Socket::m_listen()
 {
-    if (listen(Socket::_fd, SOMAXCONN) == -1)
+    if (listen(_socket.fd, SOMAXCONN) == -1)
     {
         Logger().error() << "Failed to listen on socket. errno: " << errno
                          << ":" << strerror(errno);
         exit(EXIT_FAILURE);
     }
     Logger().info() << "Listen on socket";
-    fcntl(Socket::_fd, F_SETFL, O_NONBLOCK);
+    fcntl(_socket.fd, F_SETFL, O_NONBLOCK);
     Logger().info() << "Socket set nonblock";
 }
 
@@ -58,4 +54,10 @@ void
     m_create();
     m_bind(port);
     m_listen();
+}
+
+Socket::Socket()
+{
+    _socket.len = sizeof(_socket.addr);
+    _socket.fd  = -1;
 }
