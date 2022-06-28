d58eb83c2f03c122a7e176a51c64712afaa0ffa8
snpark <atimangel@google.com>
2022-06-28 12:49:40 +0900

    주석 제거

--- srcs/ircd/ping.cpp
+++ srcs/ircd/ping.cpp
@@ -1,9 +1,12 @@
 #include "../../includes/ircd.hpp"
 
 void
+	IRCD::pong()
+{
+}
+
+void
 	IRCD::ping()
 {
-	//핑 메시지가 왔음
-	//퐁 메시지를 보내줘야함
 	m_to_client(cmd_pong_reply());
 }
--- srcs/ircd/pong.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../includes/ircd.hpp"
-
-void
-	IRCD::pong()
-{
-	//pong 메시지가 왔음 무시하면 됨
-}
